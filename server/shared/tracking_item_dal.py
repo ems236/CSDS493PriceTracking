bc8cfad5d97b42b183acf5e103010cef42e9de08
Ellis <ems236@case.edu>
2020-10-24 17:30:53 -0400

    method stubs with type hints

--- server/shared/tracking_item_dal.py
+++ server/shared/tracking_item_dal.py
@@ -11,5 +11,31 @@ class TrackingItemDAL:
     def __init__(self, isTest=False):
         self.connectionString = CONN_STR if not isTest else TEST_CONN_STR
     
+    def createItem(self, item: TrackingItem, userId: str):
+        pass
+
+    def deleteItem(self, itemId: int, userId: str):
+        pass
+
+    def updateItem(self, item: TrackingItem, userId: str):
+        pass
+
+    def logPrice(self, itemId: int, price: float, primePrice: float):
+        pass
+
+    def notificationItems(self, userId: str):
+        pass
+
+    def similarItems(self, userId: str, itemId: int):
+        pass
+
+    def updateSortOrder(self, userId: str, itemIds: list, sortOrder: list):
+        pass
+
+    def registerSimilar(self, item: SimilarItem):
+        pass
+
+    def itemsToScrape(self):
+        pass
 
      
