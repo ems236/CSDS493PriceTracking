cb20c428f0b2f5c5cc7522cde7f2a985cdb56a94
cxw580 <72585329+cxw580@users.noreply.github.com>
2020-10-25 07:59:59 +0800

    Update TrackingItem.js
    Add name and imgUrl

--- extension/js/TrackingItem.js
+++ extension/js/TrackingItem.js
@@ -1,8 +1,10 @@
 // This is a data class that represents a tracked item
 class TrackingItem {
-    constructor(id, amazonUrl, priceThreshold, timeThreshold, sampleFrequency, priceHistory){
+    constructor(id, name, amazonUrl, imgUrl, priceThreshold, timeThreshold, sampleFrequency, priceHistory){
         this.id = id;
+        this.name = name;
         this.amazonUrl = amazonUrl;
+        this.imgUrl = imgUrl;
         this.priceThreshold = priceThreshold;
         this.timeThreshold =  timeThreshold;
         this.sampleFrequency = sampleFrequency;
