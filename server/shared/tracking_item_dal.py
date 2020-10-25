99f3069653de03355d5526c7625983f883c56a56
Ellis <ems236@case.edu>
2020-10-24 21:15:51 -0400

    runsql command works

--- server/shared/tracking_item_dal.py
+++ server/shared/tracking_item_dal.py
@@ -1,16 +1,41 @@
+from functools import wraps
+
+import psycopg2
+
 from .similar_item import SimilarItem
 from .logged_price import LoggedPrice
 from .tracking_item import TrackingItem
 
-TEST_CONN_STR = ""
-CONN_STR = ""
+TEST_CONN_STR = {
+    'host': '127.0.0.1',
+    'port': '5432',
+    'user': 'webappuser',
+    'password': 'webappuser',
+    'dbname': 'test'
+}
 
-print(LoggedPrice(1, 1, 1))
+CONN_STR = {
+    'host': '127.0.0.1',
+    'port': '5432',
+    'user': 'webappuser',
+    'password': 'webappuser',
+    'dbname': 'pricetracking'
+}
 
 class TrackingItemDAL:
     def __init__(self, isTest=False):
         self.connectionString = CONN_STR if not isTest else TEST_CONN_STR
     
+    def run_sql(self, sql, params, cursor_func=None):
+        with psycopg2.connect(**self.connectionString) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(sql, params)
+                if cursor_func is not None:
+                    return cursor_func(cursor)
+                else:
+                    #means the query succeeded
+                    return True
+
     def createItem(self, item: TrackingItem, userId: str):
         pass
 
