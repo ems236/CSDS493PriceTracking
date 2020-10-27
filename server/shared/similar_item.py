c2760dc2f477d90e7ce920cd4f20004b5f0c3822
Mei Wong <d881017+meiwong326@users.noreply.github.com>
2020-10-27 14:14:25 -0400

    Add price field to SimilarItem

--- server/shared/similar_item.py
+++ server/shared/similar_item.py
@@ -1,14 +1,16 @@
 class SimilarItem:
-    def __init__(self, itemUrl, referrerItemId, name, imgUrl):
+    def __init__(self, itemUrl, referrerItemId, name, imgUrl, price):
         self.itemUrl = itemUrl
         self.referrerItemId = referrerItemId
         self.name = name
         self.imgUrl = imgUrl
+        self.price = price
 
     def __eq__(self, other):
         if isinstance(other, SimilarItem):
             return (self.itemUrl == other.itemUrl 
                 and self.imgUrl == other.imgUrl 
                 and self.name == other.name
-                and self.referrerItemId == other.referrerItemId)
+                and self.referrerItemId == other.referrerItemId
+                and self.price == other.price)
         return False
