306118ba970a6d5b106a17231dde9d9bbce0fbf8
cxw580 <72585329+cxw580@users.noreply.github.com>
2020-11-05 10:15:51 +0800

    Update generate-page.js

--- extension/js/generate-page.js
+++ extension/js/generate-page.js
@@ -6,7 +6,7 @@
 */
 
     //Randomly generates carousel item based on the amount of card items
-    var random = Math.floor(Math.random() * 10) + 1;
+    var random = 3;
     //alert("This tab has " + random + " card items.");
 
     // Page number = number of items / 3
@@ -21,11 +21,11 @@
         card += "      <a href=\"#\" id=\"card-link-title-" + i + "\" target=\"_blank\">";
         card += "         <img class=\"card-img-top\" src=\"...\" alt=\"Card image cap\" id = \"img-card" + i + "\">";
         card += "      </a>";
-        card += "      <div class=\"card-body\">";
+        card += "      <div class=\"card-body\" style=\"text-align:center\">";
         card += "         <h5 class=\"card-title\" id = \"card-title" + i + "\">Product Name</h5>";
         card += "         <p class=\"card-text\">";
         card += "            Current Price:";
-        card += "            <span id=\"current-price-card" + i + "\">";
+        card += "            <span class = \"card-price\" id=\"current-price-card" + i + "\">";
         card += "            ---";
         card += "            </span>";
         card += "            Lowest Price:";
@@ -34,11 +34,33 @@
         card += "            </span>";
         card += "            <h6>Shipping included, if any</h6>";
         card += "         </p>";
+	card += "         <!-- trigger modal -->";
+        card += "         <a data-toggle=\"modal\" href=\"#detailModal\" class=\"card-link\" id=\"card-link-footer-"+ i + "\">Detail</a>";
+	card += "         <p></p>";
+	card += "         <a data-toggle=\"modal\" href=\"#deleteModal-" + i + "\" class=\"card-link\" id=\"card-link-delete-" + i + "\">Delete</a>";
+	card += "         <!-- Modal for delete-->";
+	card += "         <div class=\"modal\" id=\"deleteModal-" + i + "\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">";
+	card += "           <div class=\"modal-dialog\" role=\"document\">";
+	card += "             <div class=\"modal-content\">";
+	card += "               <div class=\"modal-header\">";
+	card += "                 <h5 class=\"modal-title\">Confirm delete</h5>";
+	card += "                 <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">";
+	card += "                   <span aria-hidden=\"true\">&times;</span>";
+	card += "                 </button>";
+	card += "               </div>";
+	card += "               <div class=\"modal-body\">";
+	card += "                 <p>Delete this item from the product list?</p>";
+	card += "               </div>";
+	card += "               <div class=\"modal-footer\">";
+	card += "                 <button type=\"button\" class=\"btn btn-primary\" name=\"delete-btn\">Delete</button>";
+	card += "                 <button type=\"button\" class=\"btn btn-secondary\" name=\"cancel-btn\" data-dismiss=\"modal\">Cancel</button>";
+	card += "               </div>";
+	card += "             </div>";
+	card += "           </div>";
+	card += "         </div>";
         card += "      </div>";
-        card += "      <div class=\"card-footer\">";
+        card += "      <div class=\"card-footer\" style=\"text-align:center\">";
         card += "         <small class=\"text-muted\">Last updated 3 mins ago</small>";
-        card += "         <a href=\"#\" class=\"card-link\" id=\"card-link-footer-" + i + "\" target=\"_blank\">Detail</a>";
-        card += "         <a href=\"#\" class=\"card-link\">Delete</a>";
         card += "      </div>";
         card += "   </div>";
 
@@ -57,8 +79,8 @@
 
     // add more card page
     for (let j = 1; j < page; j++) {
-      $('<div class="carousel-item" id="carousel-item-' + j + '"></div>').appendTo('.carousel-inner');
-      $('<li data-target="#demo" data-slide-to="' + j + '"></li>').appendTo('.carousel-indicators');
+      $('<div class="carousel-item" id="carousel-item-' + j + '"></div>').appendTo('.carousel-inner'); // shouuld be id
+      $('<li data-target="#demo" data-slide-to="' + j + '"></li>').appendTo('.carousel-indicators');  // should be id
       
       // add respective cards
       var cardgroup = j + 1;
@@ -80,3 +102,5 @@
       }
 
     }
+	
+	
