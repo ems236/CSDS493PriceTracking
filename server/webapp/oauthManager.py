e025e333f2c5b8fb941017806a5ed9855269bce0
Ellis <ems236@case.edu>
2020-11-05 01:30:30 -0500

    verify is way easier

---
+++ server/webapp/oauthManager.py
@@ -0,0 +1,20 @@
+from functools import wraps
+
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from flask import abort, request 
+
+from server.webapp import app
+
+
+def idForToken(request):
+    if request.json is None or "idToken" not in request.json:
+        abort(403)
+
+    token = request["idToken"]
+
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), app.config["CLIENT_ID"])
+        return idinfo['sub']
+    except ValueError:
+        abort(403)
