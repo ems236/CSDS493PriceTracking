b32400e63753ac9f8dbcce970b0f2950430cfa05
cxw580 <72585329+cxw580@users.noreply.github.com>
2020-11-06 07:53:01 +0800

    Update inject.js
    Fix wrongly displayed injected message

--- extension/js/inject.js
+++ extension/js/inject.js
@@ -4,8 +4,13 @@
   TODO: location may have surprising difference
 */
 $.get(chrome.runtime.getURL('InjectedContentUI.html'), function(data) {
-	// This location can be changing 
-    $("#exports_desktop_qualifiedBuybox_atc_feature_div").before(data);
+    // This location can be changing 
+    // Check if the track button actually gets injected
+    if($('#exports_desktop_qualifiedBuybox_atc_feature_div').length)
+    {
+      $("#exports_desktop_qualifiedBuybox_atc_feature_div").before(data);
+      console.log('Price track button injected.');
+    }
 });	
 
-console.log('Price track button injected.');
+
