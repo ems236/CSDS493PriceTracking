644960b131ce63b130a8d2260563de9fed91f367
cxw580 <72585329+cxw580@users.noreply.github.com>
2020-11-07 00:04:46 +0800

    add a hidden field to keep token

--- extension/js/inject.js
+++ extension/js/inject.js
@@ -3,14 +3,20 @@
   
   TODO: location may have surprising difference
 */
-$.get(chrome.runtime.getURL('InjectedContentUI.html'), function(data) {
-    // This location can be changing 
-    // Check if the track button actually gets injected
-    if($('#exports_desktop_qualifiedBuybox_atc_feature_div').length)
-    {
-      $("#exports_desktop_qualifiedBuybox_atc_feature_div").before(data);
-      console.log('Price track button injected.');
-    }
-});	
 
-
+chrome.runtime.sendMessage({
+  message: 'login'
+}, function(response) {
+  if (response.success) {
+    //console.log(response.token);
+    $.get(chrome.runtime.getURL('InjectedContentUI.html'), function(data) {
+      // This location can be changing 
+      // Check if the track button actually gets injected
+	  data += '<input type="hidden" id="tokenId" name="tokenId" value="' + response.token + '">';
+      if ($('#exports_desktop_qualifiedBuybox_atc_feature_div').length) {
+        $("#exports_desktop_qualifiedBuybox_atc_feature_div").before(data);
+        console.log('Price track button injected.');
+      }
+    });
+  }
+});
